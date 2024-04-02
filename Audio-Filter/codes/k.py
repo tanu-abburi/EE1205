13cedf3be15976adbdaf21147237466b0958c3b5
tanu-abburi <155563399+tanu-abburi@users.noreply.github.com>
2024-04-02 23:21:24 +0530

    Add files via upload

--- Audio-Filter/codes/k.py
+++ Audio-Filter/codes/k.py
@@ -1,20 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
+import soundfile as sf
 
-k = 12
-h = np.zeros(k)
-h[0] = 1
-h[1] = -0.5*h[0]
-h[2] = -0.5*h[1] + 1
+#read .wav file 
+input_signal,fs = sf.read('tanusha.wav') 
 
-for n in range(3,k-1):
-		h[n] = -0.5*h[n-1]
+#sampling frequency 
+sampl_freq=fs
+
+#order of the filter
+order=4
+
+#cutoff frquency 1kHz
+cutoff_freq=1000.0 
+
+#digital frequency
+Wn=2*cutoff_freq/sampl_freq  
+
+# b and a are numerator and denominator polynomials respectively
+b, a = signal.butter(order, Wn, 'low') 
+
+#DTFT
+def H(z):
+	num = np.polyval(b,z**(-1))
+	den = np.polyval(a,z**(-1))
+	H = num/den
+	return H
+		
+#Input and Output
+omega = np.linspace(0,np.pi,100)
 
 #subplots
-plt.stem(range(0,k),h)
-plt.title('Impulse Response Definition')
-plt.xlabel('$n$')
-plt.ylabel('$h(n)$')
+plt.plot(omega, abs(H(np.exp(1j*omega))))
+plt.xlabel('$\omega$')
+plt.ylabel('$|H(e^{\jmath\omega})| $')
 plt.grid()
-
-plt.savefig('hndef.png')
+plt.savefig("Filter_Response")
